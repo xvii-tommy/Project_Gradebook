746440758d50cc3b90052ec76bcbb2716643684e
xvii-tommy <thomas.easterbrook97@gmail.com>
2024-02-05 18:22:49 +0000

    Reading in more files using Pandas and formatting data in tables

--- Gradebook.py
+++ Gradebook.py
@@ -11,5 +11,19 @@ roster = pd.read_csv(DATA_FOLDER / "roster.csv",
                      usecols=["Section", "Email Address", "NetID"],
                      index_col="NetID")
 
-print(roster)
+hw_ex_grades = pd.read_csv( DATA_FOLDER / "hw_exam_grades.csv",
+                            converters={"SID": str.lower},
+                            usecols=lambda x: "Submission" not in x,
+                            index_col="SID",)
+
+quiz_grades = pd.DataFrame()
+for file_path in DATA_FOLDER.glob("quiz_*_grades.csv"):
+    quiz_name = " ".join(file_path.stem.title().split("_")[:2])
+    quiz = pd.read_csv(
+        file_path,
+        converters={"Email": str.lower},
+        index_col=["Email"],
+        usecols=["Email", "Grade"],
+    ).rename(columns={"Grade": quiz_name})
+    quiz_grades = pd.concat([quiz_grades, quiz], axis=1)
 
